e0f85b7e4719eb7de6ef919e3b18f5714b8a2c8d
Atrik Das <atrikdas@gmail.com>
2021-03-13 17:54:14 +0800

    random maze

--- src/entities/CreateEnvironment.java
+++ src/entities/CreateEnvironment.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.Random;
+
 public class CreateEnvironment {
 	
 	private StateModel[][] maze = null;
@@ -13,6 +15,15 @@ public class CreateEnvironment {
 		this.row = row;
 		this.column = column;
 		setMaze();
+	}
+	
+	//Constructor for CreateRandomEnvironment
+	public void CreateRandomEnvironment(int row, int column) {
+
+		maze = new StateModel[Constants.WIDTH][Constants.HEIGHT];
+		this.row = row;
+		this.column = column;
+		setRandomMaze();
 	}
 	
 	//Getter for maze
@@ -55,5 +66,47 @@ public class CreateEnvironment {
 			maze[Integer.parseInt(coords[0])][Integer.parseInt(coords[1])].setIsWall(true);
 		}
 	}
+	
+	//Setter for random maze (builds the random maze)
+		public void setRandomMaze() {
+			
+			Random rand = new Random();
+			
+			// Initialize all squares with reward of -0.040
+			for(int row = 0; row < Constants.WIDTH; row++) {
+				for(int col = 0; col < Constants.HEIGHT; col++) {
+					maze[col][row] = new StateModel(Constants.EMPTY_SQUARE);
+				}
+			}
+			
+			//Randomly assign each square with green, brown, and wall rewards
+			for (int row = 0; row < Constants.WIDTH; row++){
+				for(int col = 0; col < Constants.HEIGHT; col++) {
+					switch(rand.nextInt(3)) {
+					case 0:
+						if (rand.nextInt(2) == 0) {
+						maze[row][col].setReward(Constants.GREEN_SQUARE);
+						}
+						break;
+					case 1:
+						if (rand.nextInt(2) == 0) {
+						maze[row][col].setReward(Constants.BROWN_SQUARE);
+						}
+						break;
+					case 2:
+						if (rand.nextInt(2) == 0) {
+						maze[row][col].setReward(Constants.WALL_SQUARE);
+						maze[row][col].setIsWall(true);
+						}
+						break;
+					}					
+				}
+			}
+			
+			//If the agent coordinate is set with something other than empty reward, reset it back to empty reward
+			String [] coordsAgent = Constants.AGENT_COORD.split(",");
+			maze[Integer.parseInt(coordsAgent[0])][Integer.parseInt(coordsAgent[1])].setReward(Constants.EMPTY_SQUARE);
+			
+		}
 
 }
